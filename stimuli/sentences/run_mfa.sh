e08e1e022191f936e737f7d5e7f6487c250e3640
Yair LAKRETZ <yair.lakretz@gmail.com>
2022-08-15 12:58:14 +0200

    praat generator for textgrids and wavs

--- stimuli/sentences/run_mfa.sh
+++ stimuli/sentences/run_mfa.sh
@@ -1,10 +1,10 @@
 # The conda environment must be activated before running
 
 path2mfa='/home/yl254115/miniconda3/envs/aligner/bin/mfa'
-path2input='/home/yl254115/projects/LPC_2022/Decoder/stimuli/sentences/mfa_input'
+path2input='/home/yl254115/projects/LPC_2022/Decoder/stimuli/sentences/mfa'
 path2dict='/home/yl254115/Documents/MFA/pretrained_models/dictionary/french_mfa.dict'
 language_model='french_mfa'
-path2output='/home/yl254115/projects/LPC_2022/Decoder/stimuli/sentences/mfa_output'
+path2output='/home/yl254115/projects/LPC_2022/Decoder/stimuli/sentences/mfa'
 
 echo 'The following commands should be launched:'
 echo '------------------------------------------'
